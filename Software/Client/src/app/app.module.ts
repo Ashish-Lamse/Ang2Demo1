a4175effe4bfb107d316bcc072ceb0076a6de666
Ashish-Lamse <ashish.lamse@india.semanticbits.com>
2017-07-03 15:14:02 +0530

    First commit

---
+++ Software/Client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CircularComponent } from './circular/circular.component';
+import { SearchComponent } from './search/search.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CircularComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Software/Client/src/app/circular/circular.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circular',
+  templateUrl: './circular.component.html',
+  styleUrls: ['./circular.component.css']
+})
+export class CircularComponent implements OnInit {
+
+  public circularTxt="This is circular txt UI.";
+
+  public bikesList=['Hero spelender',
+    'Honda shine',
+    'Bajaj discover',
+  'Tvs Victor'];
+
+  constructor() { }
+
+  clickMe(){
+    this.circularTxt="Click event is getting fire";
+  }
+
+  ngOnInit() {
+  }
+
+}
